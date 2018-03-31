45d8751d4a5c337d0661e4cfc8e11c2197b41c19
Thalison Wilker <thalison.wilker@gmail.com>
2018-03-31 03:01:29 -0300

    Adicionando o bootstrap para ficar bonitinnho

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-bt">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="<?=base_url('assets/css/')?>bootstrap.min.css">
 </head>
 <body>
 	<?php $this->load->view($view) ?>
